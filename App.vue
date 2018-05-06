6a95297b483c3d44b4e93624de60d06ac1d38403
xiaojiatao <924529755@qq.com>
2018-05-06 23:06:48 +0800

    Add files via upload

---
+++ App.vue
@@ -0,0 +1,74 @@
+<template>
+	<div id="app">
+		<transition :name="transitionName">
+			<navigation>
+				<router-view class="child-view"></router-view>
+			</navigation>
+		</transition>
+	</div>
+</template>
+
+<script>
+	export default {
+		name: 'App',
+		data () {
+			return {
+				transitionName: ''
+			}
+		},
+//		watch: {
+//			'$route' (to, from) {
+//				let mb = typeof(to.meta) == 'number' ? to.meta : 1;       // 目标
+//				let fyd = typeof(from.meta) == 'number' ? from.meta : 1;  // 源
+//				this.transitionName = mb > fyd ? 'slide-right' : 'slide-left'
+//			}
+//		},
+		created () {
+			this.$navigation.on('forward', (to, from) => {
+		    this.transitionName = 'slide-right'
+		 })
+		 this.$navigation.on('back', (to, from) => {
+		    this.transitionName = 'slide-left'
+		 })
+		 this.$navigation.on('replace', (to, from) => {
+		    this.transitionName = 'slide'
+		 })
+		}
+	}
+</script>
+
+<style>
+	html, body, div {
+		height: 100%;
+	}
+
+	body {
+		margin: 0;
+	}
+
+	#app {
+		font-family: 'Avenir', Helvetica, Arial, sans-serif;
+		-webkit-font-smoothing: antialiased;
+		-moz-osx-font-smoothing: grayscale;
+		text-align: center;
+		color: #2c3e50;
+	}
+
+	.child-view {
+  		position: absolute;
+  		width: 100%;
+  		transition: all .5s cubic-bezier(.55,0,.1,1);
+	}
+
+	.slide-left-enter, .slide-right-leave-active {
+  		opacity: 0;
+  		-webkit-transform: translate(30px, 0);
+  		transform: translate(30px, 0);
+	}
+	
+	.slide-left-leave-active, .slide-right-enter {
+  		opacity: 0;
+  		-webkit-transform: translate(-30px, 0);
+  		transform: translate(-30px, 0);
+	}
+</style>
---
+++ main.js
@@ -0,0 +1,37 @@
+// The Vue build version to load with the `import` command
+// (runtime-only or standalone) has been set in webpack.base.conf with an alias.
+import Vue from 'vue'
+import App from './App'
+import router from './router'
+import VueAMap from 'vue-amap'
+
+import axios from 'axios'
+
+//import ElementUI from 'element-ui'
+//import 'element-ui/lib/theme-chalk/index.css'
+import Navigation from 'vue-navigation'
+
+//import 'animate.css/animate.min.css'
+import 'vue2-animate/dist/vue2-animate.min.css';
+
+
+
+Vue.use(Navigation, {router})
+//Vue.use(ElementUI);
+Vue.use(VueAMap)
+VueAMap.initAMapApiLoader({
+  key: '5e9f4907fb6d8292e9bafd5617f4da6f',
+  plugin: ['AMap.Autocomplete', 'AMap.PlaceSearch', 'AMap.Scale', 'AMap.OverView', 'AMap.ToolBar', 'AMap.MapType', 'AMap.PolyEditor', 'AMap.CircleEditor'],
+  // 默认高德 sdk 版本为 1.4.4
+  v: '1.4.4'
+});
+Vue.config.productionTip = false
+
+
+/* eslint-disable no-new */
+new Vue({
+  el: '#app',
+  router,
+  components: { App },
+  template: '<App/>'
+})
